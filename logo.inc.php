1434d3f65a2ca959618a49fd6c9aa16ee239812e
negodyi <118271175+negodyi@users.noreply.github.com>
2022-11-15 17:58:25 +0300

    Add files via upload

---
+++ logo.inc.php
@@ -0,0 +1,4 @@
+
+<div class="logo">
+<img src="img/papai.jpg" alt="php">
+</div>
---
+++ menu.inc.php
@@ -0,0 +1,12 @@
+<nav>
+
+    <ul>
+        <li><a href="https://ru.wikipedia.org/wiki/%D0%92%D0%B0%D0%BB%D1%8C%D0%B4%D0%B5%D0%BC%D0%B0%D1%80"> About my </a></li>
+        <li><a href="https://rare-gallery.com/uploads/posts/1111656-digital-art-Popeye-night-3D-hat-clouds-tattoo-Moon-realistic-sailors-mythology-pipes-sailor-darkness-screenshot-computer-wallpaper.jpg"> My portfolio </a></li>
+        <li><a href="#"> My articles </a></li>
+        <li><a href="#"> Links </a></li>
+        <li><a href="https://catherineasquithgallery.com/uploads/posts/2021-03/1614550105_12-p-memi-na-belom-fone-13.png"> My Contacts </a></li>
+    </ul>
+
+
+</nav>
